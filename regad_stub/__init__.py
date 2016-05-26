5b01843f9f5b95a791d6ce1e7992f4919243a62d
Tom Galvin <tom.galvin@landregistry.gov.uk>
2016-05-26 17:03:57 +0100

    Simplify routing.

--- regad_stub/__init__.py
+++ regad_stub/__init__.py
@@ -1,16 +1,21 @@
 import os
-from flask import Flask
+
+
+from flask import Flask, jsonify
 
 
 app = Flask(__name__)
 
-from .register_adapter import views
-
 
 app.config.from_object(os.environ.get('SETTINGS'))
-app.register_blueprint(views.register_adapter, url_prefix='/register-adapter')
 
 
 @app.route("/")
 def index():
     return "regad_stub"
+
+
+@app.route('/get-proprietor-names/<title_number>', methods=["GET"])
+def get_proprietor_names(title_number):
+    return jsonify({'proprietor_names':
+                      ['John Smith', 'John Smith', 'Janet Smith']})
