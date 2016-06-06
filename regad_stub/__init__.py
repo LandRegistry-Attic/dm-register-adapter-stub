d2553eae878ad16624cb65b1bde9a5fca2d5b8b0
Tom Galvin <tom.galvin@landregistry.gov.uk>
2016-06-06 15:45:19 +0000

    Two Ann Smiths.

--- regad_stub/__init__.py
+++ regad_stub/__init__.py
@@ -17,9 +17,10 @@ def index():
 
 @app.route('/get-proprietor-names/<title_number>', methods=["GET"])
 def get_proprietor_names(title_number):
-    proprietor_names  = {'GR314108' : ["Ann Smith", "Ann Other"],
+    proprietor_names  = {'GR314108' : ["Ann Smith", "Ann Smith"],
 	                 'GR515835' : ["Ann Smith"],
 	                 'GR517788' : ["Ann Marie-Jones Smith", "Belinda Blue", "Charles John Morris Smith Jones"],
+
                          'AV182773' : ["Lisa I'anson", "Ann Other"],
                          'GR517730' : ["Liam Tremoille"]}.get(title_number, [])
     return jsonify({'proprietor_names': proprietor_names})
